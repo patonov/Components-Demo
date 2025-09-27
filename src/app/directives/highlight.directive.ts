aafbdf52f957941bf531f14cba1aec95a2142090
Nikolay Patonov <patonec@mail.bg>
2025-09-27 22:00:55 +0300

    amend008002

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -13,5 +13,10 @@ export class HighLightDirective implements OnInit {
         console.log('directive is loaded.');
         console.log(this.elRef.nativeElement);
         this.renderer.setStyle(this.elRef.nativeElement, 'background', 'pink');
+        this.renderer.listen(this.elRef.nativeElement, 'mouseenter', this.mouseEventHandler.bind(this));
+    }
+
+    mouseEventHandler() {
+      this.renderer.setStyle(this.elRef.nativeElement, 'background', 'green');  
     }
 }
