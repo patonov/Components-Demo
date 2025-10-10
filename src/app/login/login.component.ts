4c82ce5c007972a8672639235fb7057e3b8bad5c
Nikolay Patonov <patonec@mail.bg>
2025-10-10 21:19:50 +0300

    amend010007

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
