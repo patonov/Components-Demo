cc3cc0a2d9f7e7285c35cf68d0979addb41bb1bb
Nikolay Patonov <patonec@mail.bg>
2026-01-31 22:48:11 +0200

    amend021001

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { WrapperComponent } from './wrapper/wrapper.component';
-import { UsersListComponent } from './users-list.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LazyComponent } from './lazy/lazy.component';
+// import { WrapperComponent } from './wrapper/wrapper.component';
+// import { UsersListComponent } from './users-list.component';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-    { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: WrapperComponent },
-    { path: 'contacts', component: UsersListComponent},
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    // { path: "", redirectTo: "/home", pathMatch: "full" },
+    // { path: 'home', component: HomeComponent },
+    // { path: 'about', component: WrapperComponent },
+    // { path: 'contacts', component: UsersListComponent},
+    // { path: 'login', component: LoginComponent },
+    // { path: 'register', component: RegisterComponent }
+
+    {
+        path: '',
+        pathMatch: 'full',
+        component: HomeComponent
+    },
+    {
+        path: 'lazy',
+        loadChildren: ()=> import('./lazy/lazy.component').then(c => c.LazyComponent),
+    }
+
 ];
